52cdaf849a39474484507cf267ce5500df320650
Justin Im <jim023@xe-01.cs.ucr.edu>
2024-05-15 15:13:59 -0700

    Implemented outputStartMenu() and began working on main.cpp and outputTurnMenu()

--- ui.cpp
+++ ui.cpp
@@ -6,14 +6,61 @@ to terminal to prompt for more user input. */
 
 #include "ui.h"
 
+// Necessary includes for printing to terminal.
+#include <iostream>
+#include <ostream>
+using namespace std;
+
+bool ui::outputStartMenu() {
+    int userInput = 0;
+    cout << "Welcome to Combat Chess." << endl;
+    while (1) {
+        cout << "Make your selection." << endl;
+        cout << "1: Start game." << endl;
+        cout << "2: Rule book." << endl;
+        cout << "3: Quit." << endl;
+        cin >> userInput;
+        if (!cin.good()) {
+            continue;
+        }
+        // FIXME: Implement a switch statement.
+        else if (userInput == 1) {
+            cout << "Game Starto!" << endl;
+            return true;
+        }
+        else if (userInput == 2) {
+            // FIXME: Implement RuleBook. 
+            continue;
+        }
+        else {
+            break;
+        }
+    }
+    cout << "Farewell." << endl;
+    return false;
+}
+
+
 void ui::outputTurnMenu() {
-    cout << "PLAYER " << /*Game::getColor()*/ << "TURN" << endl;
+    cout << "PLAYER " << "TURN" << endl;
 
     bool flag = 1;
+    int x; int y;
     while (flag) {
-        cout << "Coordinates of a piece to be moved: " << endl;
-        // cin
-        // check for 0-7 value. if false, flag remains 1. 
+        cout << "State the location of your vassal: " << endl;
+        cin >> x;
+        if (!cin.good() || x < 0 || x > 7) {
+            cout << "Your vassal cannot flee the battlefield." << endl;
+            continue;
+        }
+        else {
+            cin >> y;
+            if (!cin.good() || y < 0 || y > 7) {
+                cout << "Your vassal cannot flee the battlefield." << endl;
+                continue;
+            }
+        }
+
         // if game::validInput from cin is invalid, flag remains 1.
         // else, flag = 0
     }
@@ -21,7 +68,7 @@ void ui::outputTurnMenu() {
     flag = 1;
 
     while (flag) {
-        cout << "Coordinates of the new piece's location: " << endl;
+        cout << "State the new location for your vassal: " << endl;
         // cin
         // check for 0-7 value. if false, flag remains 1. 
         // if piece::validNewLoc from cin is invalid, flag remains 1.
--- ui.h
+++ ui.h
@@ -4,8 +4,15 @@ interact with the user. For example, the ui class reads input from
 and performs the correct actions, and the ui class outputs back
 to terminal to prompt for more user input. */
 
+/* Cycle should be as follows:
+* main: infinite while loop calling outputMenu().
+* Player chooses to make a turn.
+* Another infinite loop that calls outputTurnMenu().*/
+
+
+
 #pragma once
-#include <iostream>
+
 // #include "board.h"
 // #include "piece.h"
 
@@ -15,14 +22,28 @@ class ui {
 
 
     public:
-        // Outputs start menu from before the game starts. 
-        void outputMenu();
+        // Constructor
+        ui() {}
+        // Destructor
+        ~ui() {}
+
+
+
+        /* Outputs start menu from before the game starts. If the return statement is true, then the game begins. 
+         Otherwise, the game ends. */
+        bool outputStartMenu();
+
         // Outputs the menu that occurs during the game cycle.
         /* Should have interface for turns, such as selecting
         a piece, and asking for the move location. Since it is 
         one touch chess, the player should not be allowed to revert
         and choose a different piece. */
         void outputTurnMenu();
+
+
+
+        /* Gets the user input, and then sends it to board. If board states that there is no piece, then we exit out of the function
+         and then start again from the output. */
 
         // Basically calls printBoard from the game class. 
         void printGame();
