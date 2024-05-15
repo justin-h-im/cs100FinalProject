e0781904349f6d6019269f70d513176867744853
thena-ay <athenanelson2005@gmail.com>
2024-05-15 14:44:52 -0700

    Add more Board functions

--- include/Board.hpp
+++ include/Board.hpp
@@ -10,5 +10,7 @@ class Game {
 		Color getTurn();
 		void displayBoard();
 		void updateBoard(int oldX, int oldY, int newX, int newY);
+		bool verifyPieceExists(int x, int y);
+		bool verifyCoordinates(const vector<int>&, const vector<int>&);
 		bool kingStatus();
 };
