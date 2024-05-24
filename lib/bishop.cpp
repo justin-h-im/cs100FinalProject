3323e66f487680020bdb50d6cb8e2ad3216e669f
Marcus Hsieh <80995617+MarcusHsieh@users.noreply.github.com>
2024-05-24 00:06:49 -0700

    Revert "Merge pull request #26 from cs100/thena-ay/board"
    This reverts commit 6c5d9b1693ec0fa90ab06c37adc25a9db094af49, reversing
    changes made to d0c2a8e35592936ce0e4a97106fe175451a53be4.

---
+++ lib/bishop.cpp
@@ -0,0 +1,76 @@
+#include "../include/bishop.h"
+#include <cmath>
+
+/*
+  0 1 2 3 4 5 6 7
+0 R N B Q K B N R 0
+1 P P P P P P P P 1
+2 - - - - - - - - 2
+3 - - - - - - - - 3
+4 - - - - - - - - 4
+5 - - - - - - - - 5
+6 p p p p p p p p 6
+7 r n b q k b n r 7
+  0 1 2 3 4 5 6 7
+*/
+
+// [start coordinate] (x1, y1) 
+//  move to 
+// [destination coordinate] (x2, y2)
+bool Bishop::move(int x1, int y1, int x2, int y2, const Board& board) {
+    if (!isInBounds(x2, y2) || (x1 == x2 && y1 == y2)) {
+        return false;
+    }
+
+    int dx = std::abs(x2 - x1);
+    int dy = std::abs(y2 - y1);
+
+    // bishops move diagonally, so absolute difference b/n x and y coordinates must be equal
+    if (dx != dy) {
+        return false;
+    }
+
+    /* determine (diagonal) direction of bishop movement */
+    // if ( ), then 1 = right \\ else, -1 = left 
+    int xStep = (x2 > x1) ? 1 : -1;
+    // if ( ), then 1 = up \\ else, -1 = down
+    int yStep = (y2 > y1) ? 1 : -1;
+
+    /* move once, towards destination coordinate */
+    int x = x1 + xStep;
+    int y = y1 + yStep;
+
+    // check if all squares b/n start and end are empty
+    // does NOT check the actual destination coordinate (stops right before)
+    while (x != x2 && y != y2) {
+      // return false if any coordinate b/n start and end is NOT valid 
+      if (board.getStatus(x, y) != nullptr) {
+        return false;
+      }
+      // step towards destination coordinate
+      x += xStep;
+      y += yStep;
+    }
+
+    Piece* target = board.getStatus(x2, y2);
+    // if empty OR spot has piece on it
+    if (target == nullptr || target->getColor() != this->getColor()) {
+      /* athena does this in board */
+      // // move or capture
+      // if (target != nullptr) {
+      //     // capture the piece
+      //     // replace with INITIATE FIGHT later
+      //     delete target;  
+      // }
+      // board.setStatus(x2, y2, this); // destination coordinate now holds new piece
+      // board.setStatus(x1, y1, nullptr); // start coordinate is now empty
+
+      // update own coordinates
+      this->x = x2;
+      this->y = y2;
+
+      return true; // has moved
+    }
+
+    return false;
+}
---
+++ lib/rook.cpp
@@ -0,0 +1,106 @@
+#include "../include/rook.h"
+
+#include <cmath>
+#include <algorithm> 
+
+using namespace std;
+
+bool Rook::move(int x1, int y1, int x2, int y2, const Board& board) 
+{
+
+    if (x1 == x2 || y1 == y2)
+    {
+
+        int startX = min(x1, x2) + 1;
+        int endX = max(x1, x2);
+        int startY = min(y1, y2) + 1;
+        int endY = max(y1, y2);
+
+        if (x1 == x2) 
+        {
+            for (int i = startY; i < endY; ++i)
+            {
+                if (board.getStatus(x1, i) != nullptr) 
+                {
+                    return false;
+                }
+            }
+        } 
+        else 
+        {
+            for (int i = startX; i < endX; ++i) 
+            {
+                if (board.getStatus(i, y1) != nullptr) 
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+bool Rook::canCastle(const King& king, int rookX, int rookY, int kingX, int kingY, const Board& board) const
+ {
+
+    if (hasMoved || king.hasMoved) 
+    {
+        return false;
+    }
+
+    if (kingY != rookY) 
+    {
+        return false;
+    }
+
+    if ((kingX != 4) || (rookX != 0 && rookX != 7))
+    {
+        return false;
+    }
+
+    int startX = min(kingX, rookX) + 1;
+    int endX = max(kingX, rookX);
+
+    for (int i = startX; i < endX; ++i)
+    {
+        if (board.getStatus(i, kingY) != nullptr)
+        {
+            return false;
+        }
+    }
+
+    for (int i = kingX; i != (rookX > kingX ? kingX + 2 : kingX - 2); i += (rookX > kingX ? 1 : -1)) 
+    {
+        if (isSquareUnderAttack(i, kingY, king.getColor(), board)) 
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool Rook::isSquareUnderAttack(int x, int y, Color color, const Board& board) const 
+{
+    // Iterate over all pieces on the board
+    for (int i = 0; i < 8; ++i) 
+    {
+        for (int j = 0; j < 8; ++j) 
+        {
+            Piece* piece = board.getStatus(i, j);
+
+            if (piece != nullptr && piece->getColor() != color) 
+            {
+                if (piece->move(piece->getX(), piece->getY(), x, y, board))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
