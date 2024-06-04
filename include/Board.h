1e4435af8bb99959889016697f5af293cce51640
thena-ay <athenanelson2005@gmail.com>
2024-06-04 09:51:55 -0700

    Fix verify move to incorporate piece move

--- include/Board.h
+++ include/Board.h
@@ -9,16 +9,20 @@ class Board {
 		// board is a 2d array storing pointers to the pieces (if there is no piece it stores nullptr) 
 		Game game;
 		Piece* square[8][8];
+		// determines there a player can move whatever is at the location
+		bool verifyPieceToMove(int y, int x) const;
 	public:
 		// sets up the board to the standard start for chess
 		Board();
+		~Board();
 		// moves a piece
-		void updateBoard(int oldX, int oldY, int newX, int newY);
-		// determines there a player can move whatever is at the location
-		bool verifyPieceToMove(int x, int y) const;
+		void updateBoard(int oldY, int oldX, int newY, int newX);
 		// determines if the player is making a possible move
-		int verifyMove(int x, int y) const;
+		int verifyMove(int y, int x) const;
 		// Returns whatever piece is at that square
-		Piece* getPiece(int x, int y) const;
-		void placePiece(int x, int y, Piece* piece);
+		Piece* getPiece(int y, int x) const;
+		// places a piece (for pawn promotion)
+		void placePiece(int y, int x, Piece* piece);
+		// sets the game
+		void setGame(Game* game);
 };
--- lib/Board.cpp
+++ lib/Board.cpp
@@ -16,35 +16,43 @@ using std::endl;
 // Constructor sets up the Board
 Board::Board() {
 	// set up black pieces
-	square[0][0] = new Rook(Color::BLACK, 0, 0);
-	square[0][1] = new Knight(Color::BLACK, 0, 1);
-	square[0][2] = new Bishop(Color::BLACK, 0, 2);
-	square[0][3] = new Queen(Color::BLACK, 0, 3);
-	square[0][4] = new King(Color::BLACK, 0, 4);
-	square[0][5] = new Bishop(Color::BLACK, 0, 5);
-	square[0][6] = new Knight(Color::BLACK, 0, 6);
-	square[0][7] = new Rook(Color::BLACK, 0, 7);
+	square[0][0] = new Rook(Color::BLACK, 0, 0, this);
+	square[0][1] = new Knight(Color::BLACK, 0, 1, this);
+	square[0][2] = new Bishop(Color::BLACK, 0, 2, this);
+	square[0][3] = new Queen(Color::BLACK, 0, 3, this);
+	square[0][4] = new King(Color::BLACK, 0, 4, this);
+	square[0][5] = new Bishop(Color::BLACK, 0, 5, this);
+	square[0][6] = new Knight(Color::BLACK, 0, 6, this);
+	square[0][7] = new Rook(Color::BLACK, 0, 7, this);
 	for (int i = 0; i < 8; i++) {
-		square[1][i] = new Pawn(Color::BLACK, 1, i);
+		square[1][i] = new Pawn(Color::BLACK, 1, i, this);
 	}
 
 	// set up white pieces
-	square[7][0] = new Rook(Color::WHITE, 7, 0);
-	square[7][1] = new Knight(Color::WHITE, 7, 1);
-	square[7][2] = new Bishop(Color::WHITE, 7, 2);
-	square[7][3] = new Queen(Color::WHITE, 7, 3);
-	square[7][4] = new King(Color::WHITE, 7, 4);
-	square[7][5] = new Bishop(Color::WHITE, 7, 5);
-	square[7][6] = new Knight(Color::WHITE, 7, 6);
-	square[7][7] = new Rook(Color::WHITE, 7, 7);
+	square[7][0] = new Rook(Color::WHITE, 7, 0, this);
+	square[7][1] = new Knight(Color::WHITE, 7, 1, this);
+	square[7][2] = new Bishop(Color::WHITE, 7, 2, this);
+	square[7][3] = new Queen(Color::WHITE, 7, 3, this);
+	square[7][4] = new King(Color::WHITE, 7, 4, this);
+	square[7][5] = new Bishop(Color::WHITE, 7, 5, this);
+	square[7][6] = new Knight(Color::WHITE, 7, 6, this);
+	square[7][7] = new Rook(Color::WHITE, 7, 7, this);
 	for (int i = 0; i < 8; i++) {
-		square[6][i] = new Pawn(Color::WHITE, 6, i);
+		square[6][i] = new Pawn(Color::WHITE, 6, i, this);
 	}
 }
 
-void Board::updateBoard(int oldX, int oldY, int newX, int newY) {
-	square[newX][newY] = square[oldX][oldY];
-	square[oldX][oldY] = nullptr;
+Board::~Board() {
+	for (int i = 0; i < 8; ++i) {
+		for (int j = 0; j < 8; ++j) {
+			delete square[i][j];
+		}
+	}
+}
+
+void Board::updateBoard(int oldY, int oldX, int newY, int newX) {
+	square[newY][newX] = square[oldY][oldX];
+	square[oldY][oldX] = nullptr;
 }
 
 void Board::setGame(Game* game) {
@@ -52,28 +60,49 @@ void Board::setGame(Game* game) {
 }
 
 // Correcting verifyPieceToMove
-bool Board::verifyPieceToMove(int x, int y) const {
-    Piece* curr = square[x][y];
-    if (curr == nullptr) { return false; }
-    return curr->getColor() == game->getTurn();
+bool Board::verifyPieceToMove(int y, int x) const {
+	Piece* curr = square[y][x];
+	if (curr == nullptr) { return false; }
+	return curr->getColor() == game->getTurn();
 }
 
-int Board::verifyMove(int x, int y) const {
-    Piece* curr = square[x][y];
-    if (curr == nullptr) { return 0; }
-    Color currentTurn = game->getTurn();
-    if (currentTurn == Color::WHITE && curr->getColor() == Color::BLACK || currentTurn == Color::BLACK && curr->getColor() == Color::WHITE) { return 1; }
-    return -1;
+// returns -1 if a player attempts an invalid move, returns 1 if a piece initiates combat, returns 0 if a piece just moves
+int Board::verifyMove(int oldY, int oldX, int newY, int newX) const {
+	if (!verifyPieceToMove(oldY, oldX)) {
+		game->updateTurn();
+		return -1;
+	}
+
+	Piece* curr = square[oldY][oldX];
+	if (!curr->move(oldX, oldY, newX, newY)) {
+		cout << "Your troop cannot reach that location.\n";
+		game->updateTurn();
+		return -1; 
+	}
+
+	Color currentTurn = game->getTurn();
+	if (curr == nullptr) { 
+		game->updateTurn();
+		game->updateBoard(oldY, oldX, newY, newX);
+		return 0;
+	}
+	else if (currentTurn == Color::WHITE && curr->getColor() == Color::BLACK || currentTurn == Color::BLACK && curr->getColor() == Color::WHITE) { 
+		return 1;
+	}
+	else {
+		game->updateTurn();
+		return -1;
+	}
 }
 
 // Correcting getPiece
-Piece* Board::getPiece(int x, int y) const {
-    return square[x][y];
+Piece* Board::getPiece(int y, int x) const {
+	return square[y][x];
 }
 
 // allows a piece to be added to the board (for pawn promotion)
-void Board::placePiece(int x, int y, Piece* piece) {
-	if (square[x][y] == nullptr) {
-		square[x][y] = piece;
+void Board::placePiece(int y, int x, Piece* piece) {
+	if (square[y][x] == nullptr) {
+		square[y][x] = piece;
 	}
 }
