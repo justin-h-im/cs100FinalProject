43e85857d87de9772204b19b7d6403809accdf5b
justin-h-im <imjustin679@gmail.com>
2024-06-07 05:30:36 -0700

    added combat scenario test for ui

--- include/combat.h
+++ include/combat.h
@@ -42,7 +42,6 @@ public:
 
             if (attemptAttack(attacker, defender)) {
                 std::cout << " > " << colorToString(attacker->getColor()) << " " << pieceToString(attacker->getType()) << "'s attack hits!\n";
-                std::cout << " > " << "Defender's HP: " << defender->getHp() << "\n"; // REMOVE THIS
             } else {
                 std::cout << " > " << colorToString(attacker->getColor()) << " " << pieceToString(attacker->getType()) << " swings and misses!\n";
             }
